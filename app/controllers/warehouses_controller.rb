ad5aefe488fa550bffd323f5e64495d5da71974d
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-05-02 18:53:56 -0300

    Warehouse details by API

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,4 +3,10 @@ class WarehousesController < ApplicationController
     response = Faraday.get('http://localhost:3000/api/v1/warehouses')
     @warehouses = JSON.parse(response.body)
   end
+
+  def show
+    id = params[:id]
+    response = Faraday.get("http://localhost:3000/api/v1/warehouses/#{id}")
+    @warehouse = JSON.parse(response.body)
+  end
 end
--- spec/system/user_visit_homepage_spec.rb
+++ spec/system/user_visit_homepage_spec.rb
@@ -32,4 +32,26 @@ describe 'User view homepage' do
     # Assert
     expect(page).to have_content 'Nenhum galpão encontrado'
   end
+
+  it 'view warehouse details' do
+    # Arrange
+    json_data = File.read(Rails.root.join('spec/support/json/warehouses.json'))
+    fake_response = double('faraday_response', status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:3000/api/v1/warehouses').and_return(fake_response)
+
+    json_data = File.read(Rails.root.join('spec/support/json/warehouse.json'))
+    fake_response = double('faraday_response', status: 200, body: json_data)
+    allow(Faraday).to receive(:get).with('http://localhost:3000/api/v1/warehouses/1').and_return(fake_response)
+
+    # Act
+    visit root_path
+    click_on 'Aeroporto SP'
+
+    # Assert
+    expect(page).to have_content 'Galpão GRU - Aeroporto SP'
+    expect(page).to have_content 'Guarulhos'
+    expect(page).to have_content '100000 m2'
+    expect(page).to have_content 'Avenida do Aeroporto, 1000 - CEP: 15000-000'
+    expect(page).to have_content 'Galpão destinado para cargas internacionais'
+  end
 end
